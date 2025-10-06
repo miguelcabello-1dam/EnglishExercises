9568ee2d64d643121a1c9694aaa8de2cd06be738
migue <migue@Pandemonium>
2025-10-06 23:24:51 +0200

    ñ

---
+++ src/EnglishExercises/englishexercisesp2.java
@@ -0,0 +1,40 @@
+package EnglishExercises;
+
+import java.util.Scanner;
+
+public class englishexercisesp2 {
+
+	public static void main(String[] args) {
+		
+        // Create Scanner
+        Scanner sc = new Scanner(System.in);
+        
+        // price as double
+        double price;
+        // tax as double
+        double tax;
+        
+		// Display program title
+        System.out.println("=== Price Calculator ===");
+
+        // Display prompt for price
+        System.out.print("Enter the price: ");
+        // Save price
+        price = sc.nextDouble();
+
+        // Display prompt for tax
+        System.out.print("Enter the tax percentage: ");
+        // Save tax
+        tax = sc.nextDouble();
+
+        // Calculate the new price
+        price *= (1 + tax / 100);
+
+        // Display new price
+        System.out.println("The final price with tax is: " + price);
+
+        // Close the scanner
+        sc.close();
+	}
+
+}
